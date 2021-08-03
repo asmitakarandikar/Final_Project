a22ebdcdfcc22d28895e8f281f0995ac41b3e701
Asmita <asmitavk1@gmail.com>
2021-08-03 11:02:10 -0400

    Initial Commit

---
+++ GitHub_LessonTests/GitHub_LessonTests.swift
@@ -0,0 +1,33 @@
+//
+//  GitHub_LessonTests.swift
+//  GitHub_LessonTests
+//
+//  Created by Asmita Karandikar on 8/3/21.
+//
+
+import XCTest
+@testable import GitHub_Lesson
+
+class GitHub_LessonTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
