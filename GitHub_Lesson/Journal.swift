53f87ba000a42649bc55ab94c4dd38a838740014
Lisa Chou <lisaqweb@gmail.com>
2021-08-04 09:49:23 -0500

    Added table view controller/ journaling option/ seques

---
+++ GitHub_Lesson/Journal.swift
@@ -0,0 +1,12 @@
+//
+//  Journal.swift
+//  GitHub_Lesson
+//
+//  Created by Lisa Chou on 8/4/21.
+//
+
+import UIKit
+
+class Journal{
+    var name = ""
+}
---
+++ GitHub_Lesson/JournalTableViewController.swift
@@ -0,0 +1,92 @@
+//
+//  JournalTableViewController.swift
+//  GitHub_Lesson
+//
+//  Created by Lisa Chou on 8/4/21.
+//
+
+import UIKit
+
+class JournalTableViewController: UITableViewController {
+    var journals : [Journal] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        journals = createJournals()
+    }
+    func createJournals() -> [Journal] {
+
+      let swift = Journal()
+      swift.name = "Learn Swift"
+
+
+      return [swift]
+    }
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return journals.count
+    }
+
+   
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        let journal = journals[indexPath.row]
+        cell.textLabel?.text = journal.name
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
