11332ecbb784100904a703974497fd1d2cf3a13c
Lisa Chou <lisaqweb@gmail.com>
2021-08-04 14:26:52 -0500

    Added deleting journal feature

---
+++ GitHub_Lesson/FullJournalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FullJournalViewController.swift
+//  GitHub_Lesson
+//
+//  Created by Lisa Chou on 8/4/21.
+//
+
+import UIKit
+
+class FullJournalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- GitHub_Lesson/JournalTableViewController.swift
+++ GitHub_Lesson/JournalTableViewController.swift
@@ -56,17 +56,16 @@ class JournalTableViewController: UITableViewController {
     }
     */
 
-    /*
-    // Override to support editing the table view.
+   
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            journals.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
